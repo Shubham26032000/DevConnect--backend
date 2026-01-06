170448c276ec06506720cdc980437d5e64abd6c2
Shubham <shubhamvast5@gmail.com>
2026-01-06 15:31:24 +0530

    Added spring boot security JWT token based. (it uses spring-cloud-api-gateway and webflux)

---
+++ api-gateway/src/main/java/com/devconnect/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.devconnect.api_gateway.config;
+
+import com.devconnect.api_gateway.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(
+                                "auth/login",
+                                "/user/login",
+                                "/user/create",
+                                "/eureka/**"
+                        ).permitAll()
+                        .anyExchange().authenticated()
+                )
+
+                .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .build();
+    }
+}
